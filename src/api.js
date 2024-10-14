6475c8549b872334db7d058b4ae0b63eee472564
Harrisonk76 <165740611+Harrisonk76@users.noreply.github.com>
2024-10-13 22:19:00 -0700

    file update

---
+++ src/api.js
@@ -0,0 +1,8 @@
+// frontend/src/api.js
+import axios from 'axios';
+export default axios.create({
+    baseURL: 'http://localhost:5000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
